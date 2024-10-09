72b1f1ad152edeaf3b7470da6887ee18c3aaff6e
Yuan Geng Tay <tayyuangeng@gmail.com>
2024-10-09 12:04:59 +0800

    Merged SY codes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages/LandingPage.tsx'
-import './components/layout/layout.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
