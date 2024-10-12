f2ab876448d0e94f9d0bafb41681bd3b8a963b87
Kiong <Yongkionglu@gmail.com>
2024-10-12 10:54:33 +0800

    Minor changes

--- backend/Interface/IPostRepository.cs
+++ backend/Interface/IPostRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
+using backend.DTOs.Post;
 
 namespace backend.Interface
 {
     public interface IPostRepository
     {
-
+        Task<List<Post>> GetAllAsync();
+        Task<Post?> GetByIdAsync(int id);
+        Task<Post> CreateAsync(Post postModel);
+        Task<Post?> UpdateAsync(int id, UpdatePostRequestDTO postDTO);
+        Task<Post?> DeleteAsync(int id);
     }
 }
