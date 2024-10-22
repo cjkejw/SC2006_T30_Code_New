bf6fb7cae3d73d2e2338a76742af90f8a6b63209
nichtyq <nicholastanyq@gmail.com>
2024-10-22 17:10:16 +0800

    Minor changes to CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddCors(options =>{
     options.AddPolicy("AllowSpecificOrigins", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173,http://localhost:5073")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
